2e3bef52dd4343d470413c8df2fa76e970c24930
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-02-18 23:26:55 +0200

    feat: create type folder and add type file for App component

--- src/provider/Provider.tsx
+++ src/provider/Provider.tsx
@@ -1,12 +1,10 @@
+
+import { Children } from "@/types/ChildrenTypes";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactNode, useState } from "react";
+import {useState } from "react";
 
-interface ProviderProps {
-  children: ReactNode;
-}
-
-export default function Provider({ children }: ProviderProps) {
-  const [queryClient] = useState(() =>new QueryClient());
+export default function Provider({ children }: Children) {
+  const [queryClient] = useState(() => new QueryClient());
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
