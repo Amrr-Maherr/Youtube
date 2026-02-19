60c467ca6973768524e450f1932f01d05dc835aa
Amr Maher Ali <amrr.maherr24@gmail.com>
2026-02-19 21:12:56 +0200

    fix: correct Redux CategorySlice and Store errors

---
+++ src/store/CategorySlice.ts
@@ -0,0 +1,23 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+
+interface CategoryState {
+  value: string[]
+}
+
+const initialState: CategoryState = {
+  value: [],
+}
+
+export const CategorySlice = createSlice({
+  name: 'category',
+  initialState,
+  reducers: {
+    SetCategory: (state, action: PayloadAction<string[]>) => {
+      state.value = action.payload
+    },
+  },
+})
+
+export const { SetCategory } = CategorySlice.actions
+
+export default CategorySlice.reducer
